331e546cb2f836fb0e794cc920fc1f74adc16bf0
Muggy Ate <muggy8@gmail.com>
2020-08-09 08:23:04 -0700

    navigation seems ok

--- src/js/card-type-selector.js
+++ src/js/card-type-selector.js
@@ -1,13 +1,13 @@
 (function(template){
 	let controller = {}
-	let view = proxymity(template, controller)
 
 	let focus = controller.focus = function(){
 		App.currentView = view
 	}
 
-	focus()
 	App.cardTypePicker = controller
+	let view = proxymity(template, controller)
+	focus()
 })(`
 <div class="flex hcenter">
 	<h2>I want to create a...</h2>
@@ -21,19 +21,19 @@
 	</div>
 	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter">
 		<strong>Champion Lv 2</strong>
-		<div class="clickable">
+		<div class="clickable"  onclick="App.championLv2Builder.focus()">
 			<img src="./assets/champion/frame2.png" />
 		</div>
 	</div>
 	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter">
 		<strong>Spell</strong>
-		<div class="clickable">
+		<div class="clickable"  onclick="App.spellBuilder.focus()">
 			<img src="./assets/spell/frameslownone.png" />
 		</div>
 	</div>
 	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter">
 		<strong>Follower</strong>
-		<div class="clickable">
+		<div class="clickable" onclick="App.followerBuilder.focus()">
 			<img src="./assets/follower/framegemless.png" />
 		</div>
 	</div>
