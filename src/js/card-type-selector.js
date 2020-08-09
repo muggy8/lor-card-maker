148dac3aa4698a3b6281f0d9f60c30da71d44dbf
Muggy Ate <muggy8@gmail.com>
2020-08-09 07:01:56 -0700

    styleing and templating

--- src/js/card-type-selector.js
+++ src/js/card-type-selector.js
@@ -10,30 +10,30 @@
 	app.cardTypePicker = controller
 })(`
 <div class="flex hcenter">
-	<h2>Create a...</h2>
+	<h2>I want to create a...</h2>
 </div>
 <main class="gutter-trbl-.5 flex">
 	<div class="gutter-trbl-.5 box-xs-6 box-l-3 flex column vhcenter">
 		<strong>Champion Lv 1</strong>
-		<div>
+		<div class="clickable">
 			<img src="./assets/champion/frame1gem.png" />
 		</div>
 	</div>
 	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter">
 		<strong>Champion Lv 2</strong>
-		<div>
+		<div class="clickable">
 			<img src="./assets/champion/frame2.png" />
 		</div>
 	</div>
 	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter">
 		<strong>Spell</strong>
-		<div>
+		<div class="clickable">
 			<img src="./assets/spell/frameslownone.png" />
 		</div>
 	</div>
 	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter">
 		<strong>Follower</strong>
-		<div>
+		<div class="clickable">
 			<img src="./assets/follower/framegemless.png" />
 		</div>
 	</div>
