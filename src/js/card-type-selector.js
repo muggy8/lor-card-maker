0b4c6dcdd5265bbfc7f88e332118e7c46737d18b
Muggy Ate <muggy8@gmail.com>
2020-08-08 22:35:47 -0700

    sizing changes

--- src/js/card-type-selector.js
+++ src/js/card-type-selector.js
@@ -13,25 +13,25 @@
 	<h2>Create a...</h2>
 </div>
 <main class="gutter-trbl-.5 flex">
-	<div class="gutter-trbl-.5 box-xs-12 box-s-6 box-l-3 flex column vhcenter">
+	<div class="gutter-trbl-.5 box-xs-6 box-l-3 flex column vhcenter">
 		<strong>Champion Lv 1</strong>
 		<div>
 			<img src="./assets/champion/frame1gem.png" />
 		</div>
 	</div>
-	<div class="gutter-trbl-.5 box-xs-12 box-s-6 box-l-3 flex column vhcenter">
+	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter">
 		<strong>Champion Lv 2</strong>
 		<div>
 			<img src="./assets/champion/frame2.png" />
 		</div>
 	</div>
-	<div class="gutter-trbl-.5 box-xs-12 box-s-6 box-l-3 flex column vhcenter">
+	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter">
 		<strong>Spell</strong>
 		<div>
 			<img src="./assets/spell/frameslownone.png" />
 		</div>
 	</div>
-	<div class="gutter-trbl-.5 box-xs-12 box-s-6 box-l-3 flex column vhcenter">
+	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter">
 		<strong>Follower</strong>
 		<div>
 			<img src="./assets/follower/framegemless.png" />
