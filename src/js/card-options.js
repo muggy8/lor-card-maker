9a0bdb4e96ee1172d0e6d78435fe19de2e455ae0
Muggy Ate <muggy8@gmail.com>
2020-08-10 12:36:53 -0700

    change rarity works now

--- src/js/card-options.js
+++ src/js/card-options.js
@@ -3,7 +3,7 @@ App.cardOptions = (function(template, subTemplates){
 		let controller = {subTemplates}
 		controller.card = card
 
-		let rarityOptions = this.rarityOptions = [
+		let rarityOptions = controller.rarityOptions = [
 			"none",
 			"common",
 			"rare",
@@ -18,36 +18,8 @@ App.cardOptions = (function(template, subTemplates){
 	<strong>Card Configs</strong>
 </div>
 <div class="slide-up gutter-tb {:this.app.mbShowConfigs ? 'active' : '':}|{mbShowConfigs}|">
-	<label>
-		<div>
-			<strong>Mana Cost</strong>
-		</div>
-		<div class="flex gutter-b">
-			<input
-				class="box-12"
-				name="mana-cost"
-				type="number"
-				data-value="{:this.app.card.mana:}|{card.mana}|"
-				onchange="this.app.card.mana = this.valueAsNumber || 0"
-				onkeyup="this.app.card.mana = this.valueAsNumber || 0"
-			>
-		</div>
-	</label>
-	<label>
-		<div>
-			<strong>Card Name</strong>
-		</div>
-		<div class="flex gutter-b">
-			<input
-				class="box-12"
-				name="card-name"
-				type="text"
-				data-value="{:this.app.card.name:}|{card.name}|"
-				onchange="this.app.card.name = this.value || ''"
-				onkeyup="this.app.card.name = this.value || ''"
-			>
-		</div>
-	</label>
+	{:Object.prototype.hasOwnProperty.call(this.app.card, "mana") ? proxymity(this.app.subTemplates.manaCostUI, this.app) : undefined:}
+	{:Object.prototype.hasOwnProperty.call(this.app.card, "name") ? proxymity(this.app.subTemplates.cardNameUI, this.app) : undefined:}
 	{:Object.prototype.hasOwnProperty.call(this.app.card, "rarity") ? proxymity(this.app.subTemplates.raritySelectorUI, this.app) : undefined:}
 </div>
 `, {
@@ -57,8 +29,46 @@ App.cardOptions = (function(template, subTemplates){
 				<strong>Rarity</strong>
 			</div>
 			<div class="flex">
-
+				<select name="rarity" onchange="this.app.card.rarity = this.value" data-value="{:this.app.card.rarity:}|{card.rarity}|">
+					<!-- key: "rarityIndex" -->
+						<option value="{:this.app.rarityOptions[this.rarityIndex]:}">{:this.app.rarityOptions[this.rarityIndex]:}</option>
+					<!-- in: rarityOptions -->
+				</select>
 			</div>
 		</label>
-	`
+	`,
+	manaCostUI: `
+		<label>
+			<div>
+				<strong>Mana Cost</strong>
+			</div>
+			<div class="flex gutter-b">
+				<input
+					class="box-12"
+					name="mana-cost"
+					type="number"
+					data-value="{:this.app.card.mana:}|{card.mana}|"
+					onchange="this.app.card.mana = this.valueAsNumber || 0"
+					onkeyup="this.app.card.mana = this.valueAsNumber || 0"
+				>
+			</div>
+		</label>
+	`,
+	cardNameUI: `
+		<label>
+			<div>
+				<strong>Card Name</strong>
+			</div>
+			<div class="flex gutter-b">
+				<input
+					class="box-12"
+					name="card-name"
+					type="text"
+					data-value="{:this.app.card.name:}|{card.name}|"
+					onchange="this.app.card.name = this.value || ''"
+					onkeyup="this.app.card.name = this.value || ''"
+				>
+			</div>
+		</label>
+	`,
 })
