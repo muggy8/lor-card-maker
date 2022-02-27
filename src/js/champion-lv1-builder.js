2856cf9db62ed260c681712fc73d1d0c475b25cf
Muggy Ate <muggy8@users.noreply.github.com>
2022-02-27 05:53:26 -0800

    champion level up is now optional

--- src/js/champion-lv1-builder.js
+++ src/js/champion-lv1-builder.js
@@ -133,12 +133,16 @@
 				<rect id="health" width="86" height="82" x="552" y="873" opacity="0"/>
 				<text class="key-text {:proxymity.on.renderend.then(()=>this.app.wrapText(this, true, {valign: 'middle'})).catch(()=>{}):}" font-size="50" fill="#fff" stroke="#fff">${card.health}</text>
 
-				<g id="all-text-group" transform="translate(0, {: 130 - this.app.card.lvupHeight > 0 ? 130 - this.app.card.lvupHeight : 0:}|{card.lvupHeight}|)">
+				<g id="all-text-group" transform="translate(0, {: this.app.card.lvup ? ( 130 - this.app.card.lvupHeight > 0 ? 130 - this.app.card.lvupHeight : 0 ) : 174 :}|{card.lvupHeight},{card.lvup}|)">
 					<foreignObject style="background-color: rgba(0,0,0,0);" id="level-up-condition" width="560" height="130" x="60" y="720">
 						<div xmlns="http://www.w3.org/1999/xhtml" style="font-size:{:this.app.card.levelFontSize:}|{card.levelFontSize}|px; text-align: center; overflow: hidden; max-height: 100%; color: #d6946b" data-init="{:proxymity.on.renderend.then(()=>this.app.effectResize(this, 'levelFontSize')).then(()=>this.app.card.lvupHeight = this.scrollHeight):}">${controller.decorateText(card.lvup)}</div>
 					</foreignObject>
 
-					<image id="card-level-bar" width="680" height="44" x="0" y="680" xlink:href="/assets/champion/levelupbar.png"/>
+					${
+						card.lvup
+						? '<image id="card-level-bar" width="680" height="44" x="0" y="680" xlink:href="/assets/champion/levelupbar.png"/>'
+						: ""
+					}
 
 					<g id="effect-group" transform="translate(0, {: 162 - this.app.card.effectHeight > 0 ? 162 - this.app.card.effectHeight : 0:}|{card.effectHeight}|)">
 						<foreignObject style="background-color: rgba(0,0,0,0);" id="effect" width="560" height="162" x="60" y="520">
