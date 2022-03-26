08f8bd1d66f6cfe332b8131d91658095a4cbd5f1
Muggy Ate <muggy8@users.noreply.github.com>
2022-03-26 06:25:01 -0700

    added keys to lists to improve performance

--- src/js/card-type-selector.js
+++ src/js/card-type-selector.js
@@ -87,6 +87,7 @@
 	</div>
 
 	<!-- forEach: "index" -->
+	<!-- key: function(item, index, whole){ return item.id } -->
 	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter clickable" onclick="App.championLv1Builder.focus().then(_=>{this.app.transferData(App.championLv1Builder.card, this.app.savedChampions1[this.index].cardData); App.championLv1Builder.cardId = this.app.savedChampions1[this.index].id})">
 		<strong>Edit {: this.app.savedChampions1[this.index].cardData.name :}</strong>
 		<div>
@@ -94,7 +95,7 @@
 				this.app.attached && App.batchExporter.createPreview(
 					this.app.savedChampions1[this.index].cardData,
 					App.championLv1Builder,
-					this.app.savedChampions1[this.index].cardData.id,
+					this.app.savedChampions1[this.index].id,
 				)
 			:}|{attached}|
 		</div>
@@ -102,6 +103,7 @@
 	<!-- in: savedChampions1 -->
 
 	<!-- forEach: "index" -->
+	<!-- key: function(item, index, whole){ return item.id } -->
 	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter clickable" onclick="App.championLv2Builder.focus().then(_=>{this.app.transferData(App.championLv2Builder.card, this.app.savedChampions2[this.index].cardData); App.championLv2Builder.cardId = this.app.savedChampions2[this.index].id})">
 		<strong>Edit {: this.app.savedChampions2[this.index].cardData.name :}</strong>
 		<div>
@@ -109,7 +111,7 @@
 				this.app.attached && App.batchExporter.createPreview(
 					this.app.savedChampions2[this.index].cardData,
 					App.championLv2Builder,
-					this.app.savedChampions2[this.index].cardData.id,
+					this.app.savedChampions2[this.index].id,
 				)
 			:}|{attached}|
 		</div>
@@ -117,6 +119,7 @@
 	<!-- in: savedChampions2 -->
 
 	<!-- forEach: "index" -->
+	<!-- key: function(item, index, whole){ return item.id } -->
 	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter clickable" onclick="App.championLv3Builder.focus().then(_=>{this.app.transferData(App.championLv3Builder.card, this.app.savedChampions3[this.index].cardData); App.championLv3Builder.cardId = this.app.savedChampions3[this.index].id})">
 		<strong>Edit {: this.app.savedChampions3[this.index].cardData.name :}</strong>
 		<div>
@@ -124,7 +127,7 @@
 				this.app.attached && App.batchExporter.createPreview(
 					this.app.savedChampions3[this.index].cardData,
 					App.championLv3Builder,
-					this.app.savedChampions3[this.index].cardData.id,
+					this.app.savedChampions3[this.index].id,
 				)
 			:}|{attached}|
 		</div>
@@ -132,6 +135,7 @@
 	<!-- in: savedChampions3 -->
 
 	<!-- forEach: "index" -->
+	<!-- key: function(item, index, whole){ return item.id } -->
 	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter clickable" onclick="App.landmarkBuilder.focus().then(_=>{this.app.transferData(App.landmarkBuilder.card, this.app.savedLandmarks[this.index].cardData); App.landmarkBuilder.cardId = this.app.savedLandmarks[this.index].id})">
 		<strong>Edit {: this.app.savedLandmarks[this.index].cardData.name :}</strong>
 		<div>
@@ -139,7 +143,7 @@
 				this.app.attached && App.batchExporter.createPreview(
 					this.app.savedLandmarks[this.index].cardData,
 					App.landmarkBuilder,
-					this.app.savedLandmarks[this.index].cardData.id,
+					this.app.savedLandmarks[this.index].id,
 				)
 			:}|{attached}|
 		</div>
@@ -147,6 +151,7 @@
 	<!-- in: savedLandmarks -->
 
 	<!-- forEach: "index" -->
+	<!-- key: function(item, index, whole){ return item.id } -->
 	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter clickable" onclick="App.spellBuilder.focus().then(_=>{this.app.transferData(App.spellBuilder.card, this.app.savedSpells[this.index].cardData); App.spellBuilder.cardId = this.app.savedSpells[this.index].id})">
 		<strong>Edit {: this.app.savedSpells[this.index].cardData.name :}</strong>
 		<div>
@@ -154,7 +159,7 @@
 				this.app.attached && App.batchExporter.createPreview(
 					this.app.savedSpells[this.index].cardData,
 					App.spellBuilder,
-					this.app.savedSpells[this.index].cardData.id,
+					this.app.savedSpells[this.index].id,
 				)
 			:}|{attached}|
 		</div>
@@ -162,6 +167,7 @@
 	<!-- in: savedSpells -->
 
 	<!-- forEach: "index" -->
+	<!-- key: function(item, index, whole){ return item.id } -->
 	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter clickable" onclick="App.followerBuilder.focus().then(_=>{this.app.transferData(App.followerBuilder.card, this.app.savedFollowers[this.index].cardData); App.followerBuilder.cardId = this.app.savedFollowers[this.index].id})">
 		<strong>Edit {: this.app.savedFollowers[this.index].cardData.name :}</strong>
 		<div>
@@ -169,7 +175,7 @@
 				this.app.attached && App.batchExporter.createPreview(
 					this.app.savedFollowers[this.index].cardData,
 					App.followerBuilder,
-					this.app.savedFollowers[this.index].cardData.id,
+					this.app.savedFollowers[this.index].id,
 				)
 			:}|{attached}|
 		</div>
@@ -177,6 +183,7 @@
 	<!-- in: savedFollowers -->
 
 	<!-- forEach: "index" -->
+	<!-- key: function(item, index, whole){ return item.id } -->
 	<div class="gutter-trbl-.5 box-xs-6 box-m-3 flex column vhcenter clickable" onclick="App.keywordBuilder.focus().then(_=>{this.app.transferData(App.keywordBuilder.card, this.app.savedKeywords[this.index].cardData); App.keywordBuilder.cardId = this.app.savedKeywords[this.index].id})">
 		<strong>Edit {: this.app.savedKeywords[this.index].cardData.name :}</strong>
 		<div>
@@ -184,7 +191,7 @@
 				this.app.attached && App.batchExporter.createPreview(
 					this.app.savedKeywords[this.index].cardData,
 					App.keywordBuilder,
-					this.app.savedKeywords[this.index].cardData.id,
+					this.app.savedKeywords[this.index].id,
 				)
 			:}|{attached}|
 		</div>
