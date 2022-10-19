f23458dabf41982fab1a9c388bc83e869a7790be
Muggy Ate <muggy8@users.noreply.github.com>
2022-10-19 05:25:24 -0700

    rendered editor view

--- dev/app/views/card-editor.js
+++ dev/app/views/card-editor.js
@@ -1,7 +1,7 @@
-import { div } from "/Utils/elements.js"
+import { div, button } from "/Utils/elements.js"
 import loadCss from "/Utils/load-css.js"
 import useLang from "/Utils/use-lang.js"
-import { useState } from "/cdn/react" 
+import { useState, useCallback } from "/cdn/react" 
 
 
 loadCss("/Views/card-editor.css")
@@ -12,6 +12,34 @@ export default function EditorViewFactory(cardRenderer, defaultCardData){
 
         const [card, updateCard] = useState(defaultCardData)
 
-        return "placeholder"
+        
+
+        return div(
+            { id: "card-editor", className: "flex hcenter" },
+            div(
+                { className: "card-preview gutter-t-4 gutter-rl-.5 box-xs-12 box-s-8 box-m-6 box-l-4 box-xl-3" },
+                cardRenderer(card),
+                div(
+                    {className: "flex hcenter gutter-tb"},
+                    button(
+                        { className: card.id ? undefined : "hide" },
+                        translate("delete_card")
+                    ),
+                    button(
+                        translate("save_card")
+                    ),
+                    button(
+                        translate("export")
+                    ),
+                )
+            ),
+            div(
+                { className: "card-configs gutter-rl-.5 box-xs-12 box-s-8 box-m-6 box-l-4 box-xl-3" },
+                div(
+                    {className: "flex hcenter gutter-tb"},
+
+                )
+            ),
+        )
     }
 }
