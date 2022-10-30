d4e68a62de8387f77656c9e833673158b7ba6b45
Muggy Ate <muggy8@users.noreply.github.com>
2022-10-30 02:38:53 -0700

    colorful animations for the menu bar

--- dev/app/components/side-bar.js
+++ dev/app/components/side-bar.js
@@ -1,17 +1,21 @@
 import factory, { div, span } from "/Utils/elements.js"
 import { useCallback, useEffect, useState } from "/cdn/react" 
 import loadCss from "/Utils/load-css.js"
+import useLang from "/Utils/use-lang.js"
+
 
 const cssLoaded = loadCss("/Components/side-bar.css")
 
-function SidebarComponent(props){
+function SidebarComponent(){
+    const translate = useLang()
+
     const [opened, updateOpened] = useState(false)
     const toggleOpened = useCallback(()=>{
         updateOpened(!opened)
     }, [opened])
 
     return div(
-        { className: "side-bar gutter-rl-3 " + (opened ? "open" : "") },
+        { className: `side-bar card-text-universe gutter-rl-3 flex column vhcenter ${opened ? "open" : ""}` },
         div(
             {
                 className: "menu-icon flex vhcenter",
@@ -20,7 +24,23 @@ function SidebarComponent(props){
             div({className: "icon animate clickable" }, 
                 span({ className: opened ? "delete" : "menu" })
             ),
-        )
+        ),
+        div(
+            { className: "menu-option clickable gutter-tb" },
+            translate("batch_export")
+        ),
+        div(
+            { className: "menu-option clickable gutter-tb" },
+            translate("export_save")
+        ),
+        div(
+            { className: "menu-option clickable gutter-tb" },
+            translate("import_save")
+        ),
+        div(
+            { className: "menu-option clickable gutter-tb" },
+            translate("report_bug")
+        ),
     )
 
 }
