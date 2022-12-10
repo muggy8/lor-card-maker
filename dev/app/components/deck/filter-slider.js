7e02daca7acf146161348cb5e9a73eab986a6f11
Muggy Ate <muggy8@users.noreply.github.com>
2022-12-10 03:19:22 -0800

    clean up slider bar thingy

--- dev/app/components/deck/filter-slider.js
+++ dev/app/components/deck/filter-slider.js
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from "/cdn/react";
 import svgWrap from "../card-template/svg-wrap.js";
-import factory, { div } from "/Utils/elements.js";
+import factory, { div, strong } from "/Utils/elements.js";
 import linkAsset from "/Utils/load-css.js";
 import useToggle from "/Utils/use-toggle.js";
 import useLang from "/Utils/use-lang.js";
@@ -14,16 +14,20 @@ function filterCardListConfigurationComponent (props){
     return div(
         { className: `filter-slider gutter-b-1 ${expanded ? "expanded" : ""}` },
         div({ className: "flex no-wrap vcenter" }, 
-            div({ className: "grow" }, translate("filter")),
+            div({ className: "grow clickable", onClick: toggleExpanded }, 
+                strong(translate("filter"))
+            ),
             div({className: "icon clickable animate", onClick: toggleExpanded }, 
                 div({ className: expanded ? "minus" : "menu" })
-            )
+            ),
         ),
         div({ className: "options" },
-            div("placeholder"),
-            div("placeholder"),
-            div("placeholder"),
-            div("placeholder"),
+            div({ className: "gutter-rbl-.5" },
+                div("placeholder"),
+                div("placeholder"),
+                div("placeholder"),
+                div("placeholder"),
+            )
         )
     )
 }
