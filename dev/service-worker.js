42f5a930fe94d3aeabe8ea5eaf0c394f5f9ce5f0
Muggy Ate <muggy8@users.noreply.github.com>
2023-05-15 11:42:58 -0700

    hoping new pins work out

--- dev/service-worker.js
+++ dev/service-worker.js
@@ -4,7 +4,7 @@ const urlRoot = location.origin + "/"
 const CACHE_NAME = "react-cache"
 
 const esmshQueryConfigs = {
-	target: "es2015",
+	target: "es2019",
 	"no-dts": true,
 	pin: "v120",
 	// bundle: true,
