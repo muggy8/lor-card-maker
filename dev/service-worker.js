d2b61b730a6f85b76032c170e511090fd7ceccce
Muggy Ate <muggy8@users.noreply.github.com>
2023-05-15 12:11:53 -0700

    found a working pin combo I think

--- dev/service-worker.js
+++ dev/service-worker.js
@@ -4,7 +4,7 @@ const urlRoot = location.origin + "/"
 const CACHE_NAME = "react-cache"
 
 const esmshQueryConfigs = {
-	target: "es2019",
+	target: "es2020",
 	"no-dts": true,
 	pin: "v120",
 	// bundle: true,
